3c2f1568733b6a65e11b1014dd070479d00a199a
gianpaolo papaiz <gianpaolopapaiz@hotmail.com>
2021-05-26 14:31:13 -0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts ('Starting seed...')
+categories = ["chinese", "italian", "japanese", "french", "belgian"]
+i = 0
+5.times do
+	restaurant = Restaurant.new
+	restaurant.name = "Restaurant #{i}"
+	restaurant.address = "Address #{i}"
+	restaurant.phone_number = "(11)99323-111#{i}"
+	restaurant.category = categories.sample
+	restaurant.save
+	i += 1
+end
+puts ('Done seeding!')
